f6b64852d3739adb0fcd86968d194e7d4e896de8
artaylor85 <aimee.r.taylor.85@gmail.com>
2020-03-31 19:37:42 +0100

    Bug fix: delete missing entry

--- R/estimate_r_and_k.R
+++ R/estimate_r_and_k.R
@@ -1,6 +1,6 @@
 ## Mechanism to compute MLEs given fs, Ys, epsilon and distances
 estimate_r_and_k <- function(frequencies, distances, Ys,
-                             epsilon = 0.001, , rho = 7.4 * 10^(-7), kinit = 50, rinit = 0.5){
+                             epsilon = 0.001, rho = 7.4 * 10^(-7), kinit = 50, rinit = 0.5){
   ndata <- nrow(frequencies)
   ll <- function(k, r) loglikelihood_cpp(k, r, Ys, frequencies, distances, epsilon, rho)
   optimization <- optim(par = c(kinit, rinit), fn = function(x) - ll(x[1], x[2]))
