10c0e2e64ec0f45af6e55df18b4aa6191f82d0d6
artaylor85 <aimee.r.taylor.85@gmail.com>
2020-03-31 19:35:39 +0100

    Update s.t. more explicit

--- R/compute_rhat_hmm.R
+++ R/compute_rhat_hmm.R
@@ -1,8 +1,9 @@
-## Mechanism to compute MLE given fs, Ys, epsilon and distances
-compute_rhat_hmm <- function(frequencies, distances, Ys, epsilon){
+## Mechanism to compute MLEs given fs, Ys, epsilon and distances
+estimate_r_and_k <- function(frequencies, distances, Ys,
+                             epsilon = 0.001, , rho = 7.4 * 10^(-7), kinit = 50, rinit = 0.5){
   ndata <- nrow(frequencies)
-  ll <- function(k, r) loglikelihood_cpp(k, r, Ys, frequencies, distances, epsilon, rho = 7.4 * 10^(-7))
+  ll <- function(k, r) loglikelihood_cpp(k, r, Ys, frequencies, distances, epsilon, rho)
   optimization <- optim(par = c(kinit, rinit), fn = function(x) - ll(x[1], x[2]))
-  rhat <- optimization$par
-  return(rhat)
+  rkhats <- optimization$par
+  return(rkhats)
 }
