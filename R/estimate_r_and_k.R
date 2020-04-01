99f2cf397b89529077802be5d1ea38073447e3eb
artaylor85 <aimee.r.taylor.85@gmail.com>
2020-04-01 11:43:57 +0100

    Add names of returned values

--- R/estimate_r_and_k.R
+++ R/estimate_r_and_k.R
@@ -39,5 +39,6 @@ estimate_r_and_k <- function(frequencies, distances, Ys, epsilon = 0.001, rho = 
   ll <- function(k, r) loglikelihood_cpp(k, r, Ys, frequencies, distances, epsilon, rho)
   optimization <- optim(par = c(kinit, rinit), fn = function(x) - ll(x[1], x[2]))
   rkhats <- optimization$par
+  names(rkhats) <- c('khat', 'rhat')
   return(rkhats)
 }
