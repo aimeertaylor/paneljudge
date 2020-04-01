13eb9c34fbec4de0d8b30a7ad0bfdb35c58ff588
artaylor85 <aimee.r.taylor.85@gmail.com>
2020-04-01 11:44:29 +0100

    Update s.t. works

--- R/compute_r_and_k_CIs.R
+++ R/compute_r_and_k_CIs.R
@@ -1,9 +1,33 @@
-rhats_hmm_boot = as.numeric(foreach(iboot = 1:nboot, .combine = c) %dorng% {
-  Ys_boot <- simulate_Ys_hmm(frequencies, distances = dataset_$dt, k = mle_df$khat_hmm[ind], r = mle_df$rhat_hmm[ind], epsilon)
-  ll_hmm <- function(k, r) loglikelihood_cpp(k, r, Ys_boot, frequencies, dataset_$dt, epsilon, rho = 7.4 * 10^(-7))
-  optimization <- optim(par = c(50, 0.5), fn = function(x) - ll_hmm(x[1], x[2]))
-  optimization$par[2]
-})
+#' @importFrom doRNG %dorng%
+#' @examples
+#' compute_r_and_k_CIs(frequencies = frequencies$Colombia, distances = markers$dt, khat, rhat)
 
-CI <- as.numeric(quantile(rhats_hmm_boot, probs = c(0.025, 0.975)))
-if(any(is.na(CI))){stop('Bootstrap CI is NA')}
+compute_r_and_k_CIs <- function(frequencies, distances, khat, rhat,
+                                confidence = 95, nboot = 100, do_not_use_core_count = 2,
+                                epsilon = 0.001, rho = 7.4 * 10^(-7), kinit = 50, rinit = 0.5){
+
+  # Register available cores in order to run code in parallel
+  # Note that packages parallel and foreach are available through doParallel, which is imported
+  doParallel::registerDoParallel(cores = parallel::detectCores() - do_not_use_core_count)
+
+  # Generate parametric boot strap r and k estimates
+  # %dorng% is similar to %dopar%, however
+  # %dorng% loops are reproducible whereas %dopar% loops are not,
+  # %dorng% returns the whole sequence of RNG seeds as an attribute whereas %dopar% does not.
+  attr(r1, 'rng')
+  rkhats_boot = foreach::foreach(iboot = 1:nboot, .combine = rbind) %dorng% {
+    simulated_genotype_pair <- simulate_data(frequencies, distances, k = khat, r = rhat, epsilon, rho)
+    rkhats_boot <- estimate_r_and_k(frequencies, distances, Ys = simulated_genotype_pair, epsilon, rho, kinit, rinit)
+  }
+
+  # Calculata CIs as quantiles
+  alpha <- (1-confidence/100)
+  CI <- apply(rkhats_boot, 2, quantile, probs = c(alpha/2, 1-alpha/2))
+  if(any(is.na(CI))){stop('Bootstrap CI is NA')}
+
+  # Return CIs
+  return(CI)
+}
+
+
+
