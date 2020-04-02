21c71f10e4e1487e0d42092922718ddbde73606a
artaylor85 <aimee.r.taylor.85@gmail.com>
2020-04-02 14:34:49 +0100

    Add roxygen comments

--- R/compute_r_and_k_CIs.R
+++ R/compute_r_and_k_CIs.R
@@ -1,6 +1,54 @@
+###########################################################################
+#' Compute confidence intervals for relatedness and switch rate parameters
+#'
+#' For specified confidence (default 95\%), given a matrix of input frequencies
+#' and a vector of marker distances, a switch rate parameter estimate, khat, and
+#' a relatedness parameter estimate, rhat. \code{compute_r_and_k_CIs()} returns
+#' the confidence intervals around the parameter estimates. Confidence intervals
+#' are approximate. They are approximated using the parametric bootstrap draws
+#' under the HMM described in Taylor et al. 2009 The parametric bootstrap draws
+#' are generated in parallel using a specifying number of cores. The quality and
+#' compute time of the approximation scales with nboot.
+#'
+#' @param frequencies ndata (marker count) by Kmax (max cardinality over m
+#'   markers) matrix if Kt < Kmax for any t in 1:m, then fs[t,1:Kt] in (0,1) &
+#'   fs[t,(Kt+1):Kmax] = 0 Example: if Kt = 2 < Kmax = 4 then fs[t,] might look
+#'   like [0.2, 0.7, 0, 0].
+#' @param distances vector where distances[t] contains the distance between
+#'   position t and t+1 or equivalently, gendist[t-1] contains the distance
+#'   between position t-1 and t, for p > 1. s.t. only m-1 first entries of
+#'   gendist are being used.
+#' @param confidence confidence level (percentage) of confidence interval
+#'   (default 95\%)
+#' @param nboot number of parametric bootstrap estimates from which to compute
+#'   the CI. Larger values provide a better approximation but CIs will take
+#'   longer to compute.
+#' @param do_not_use_core_count number of cores to keep free on computer
+#'   (defaults to two)
+#' @param epsilon genotyping error (probability of miscall).
+#' @param rho recombination rate in probability of break point per base pair.
+#' @param kinit switch rate parameter value used to initialise loglikelihood
+#'   optimization.
+#' @param rinit relatedness parameter value used to initialise loglikelihood
+#'   optimization.
 #' @importFrom doRNG %dorng%
+#' @return Confidence interval bounds around input switch rate parameter, k, and
+#'   relatedness parameter, r.
 #' @examples
-#' compute_r_and_k_CIs(frequencies = frequencies$Colombia, distances = markers$dt, khat, rhat)
+#'
+#' # First, stimulate some data
+#' simulated_genotype_pair <- simulate_data(fs = frequencies$Colombia, gendist = markers$dt, k = 5, r = 0.25)
+#'
+#' # Second, estimate the switch rate parameter, k, and relatedness parameter, r
+#' krhat <- estimate_r_and_k(frequencies = frequencies$Colombia, distances = markers$dt, Ys = simulated_genotype_pair)
+#'
+#' # Third, compute confidence intervals (CIs)
+#' compute_r_and_k_CIs(frequencies = frequencies$Colombia, distances = markers$dt, khat = krhat['khat'], rhat = krhat['rhat'])
+#'
+#' @references Taylor, A.R., Jacob, P.E., Neafsey, D.E. and Buckee, C.O., 2019.
+#'   Estimating relatedness between malaria parasites. Genetics, 212(4),
+#'   pp.1337-1351.
+###########################################################################
 
 compute_r_and_k_CIs <- function(frequencies, distances, khat, rhat,
                                 confidence = 95, nboot = 100, do_not_use_core_count = 2,
@@ -14,7 +62,6 @@ compute_r_and_k_CIs <- function(frequencies, distances, khat, rhat,
   # %dorng% is similar to %dopar%, however
   # %dorng% loops are reproducible whereas %dopar% loops are not,
   # %dorng% returns the whole sequence of RNG seeds as an attribute whereas %dopar% does not.
-  attr(r1, 'rng')
   rkhats_boot = foreach::foreach(iboot = 1:nboot, .combine = rbind) %dorng% {
     simulated_genotype_pair <- simulate_data(frequencies, distances, k = khat, r = rhat, epsilon, rho)
     rkhats_boot <- estimate_r_and_k(frequencies, distances, Ys = simulated_genotype_pair, epsilon, rho, kinit, rinit)
@@ -26,7 +73,7 @@ compute_r_and_k_CIs <- function(frequencies, distances, khat, rhat,
   if(any(is.na(CI))){stop('Bootstrap CI is NA')}
 
   # Return CIs
-  return(CI)
+  return(t(CI))
 }
 
 
