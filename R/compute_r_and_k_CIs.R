fc0ec992936932842b674a5f190149d3277aea07
artaylor85 <aimee.r.taylor.85@gmail.com>
2020-04-06 17:43:18 +0100

    Modify notation and comments to match Taylor et al. Genetics 2019

--- R/compute_r_and_k_CIs.R
+++ R/compute_r_and_k_CIs.R
@@ -1,8 +1,8 @@
 ###########################################################################
 #' Compute confidence intervals for relatedness and switch rate parameters
 #'
-#' For specified confidence (default 95\%), given a matrix of input frequencies
-#' and a vector of marker distances, a switch rate parameter estimate, khat, and
+#' For specified confidence (default 95\%), given a matrix of input fs
+#' and a vector of marker ds, a switch rate parameter estimate, khat, and
 #' a relatedness parameter estimate, rhat. \code{compute_r_and_k_CIs()} returns
 #' the confidence intervals around the parameter estimates. Confidence intervals
 #' are approximate. They are approximated using the parametric bootstrap draws
@@ -10,11 +10,11 @@
 #' are generated in parallel using a specifying number of cores. The quality and
 #' compute time of the approximation scales with nboot.
 #'
-#' @param frequencies ndata (marker count) by Kmax (max cardinality over m
+#' @param fs ndata (marker count) by Kmax (max cardinality over m
 #'   markers) matrix if Kt < Kmax for any t in 1:m, then fs[t,1:Kt] in (0,1) &
 #'   fs[t,(Kt+1):Kmax] = 0 Example: if Kt = 2 < Kmax = 4 then fs[t,] might look
 #'   like [0.2, 0.7, 0, 0].
-#' @param distances vector where distances[t] contains the distance between
+#' @param ds vector where ds[t] contains the distance between
 #'   position t and t+1 or equivalently, gendist[t-1] contains the distance
 #'   between position t-1 and t, for p > 1. s.t. only m-1 first entries of
 #'   gendist are being used.
@@ -23,8 +23,8 @@
 #' @param nboot number of parametric bootstrap estimates from which to compute
 #'   the CI. Larger values provide a better approximation but CIs will take
 #'   longer to compute.
-#' @param do_not_use_core_count number of cores to keep free on computer
-#'   (defaults to two)
+#' @param core_count number of cores to use to do computation. Set greater than or equal to two for parallel computation.
+#' Defaults to the number detected on the machine minus one.
 #' @param epsilon genotyping error (probability of miscall).
 #' @param rho recombination rate in probability of break point per base pair.
 #' @param kinit switch rate parameter value used to initialise loglikelihood
@@ -51,38 +51,37 @@
 #' @export
 ###########################################################################
 
-compute_r_and_k_CIs <- function(frequencies, distances, khat, rhat,
-                                confidence = 95, nboot = 100, do_not_use_core_count = 2, ...){
+compute_r_and_k_CIs <- function(fs, ds, khat, rhat, confidence = 95, nboot = 100,
+                                core_count = parallel::detectCores()-1, ...){
 
   # Retrieve all additional parameters
   all_params <- list(...)
 
-  # Define a subset of additional parameters to pass to simulate_Ys()
-  # Alternatively, could use R.utils::doCall() with .ignoreUnusedArgs=TRUE
-  # instead of base::do.call(); see call to simulate_Ys() below.
+  # Define a subset of additional parameters to pass to simulate_Ys() using base::do.call(); see below.
+  # Alternatively, could use R.utils::doCall() with .ignoreUnusedArgs=TRUE.
   sim_params <- all_params
   sim_params$kinit <- NULL
   sim_params$rinit <- NULL
 
   # Register available cores in order to run code in parallel
   # Note that packages parallel and foreach are available through doParallel, which is imported
-  doParallel::registerDoParallel(cores = parallel::detectCores() - do_not_use_core_count)
+  doParallel::registerDoParallel(cores = core_count)
 
-  # Generate parametric boot strap r and k estimates
+  # Generate parametric bootstrap draw of r and k estimates
   # %dorng% is similar to %dopar%, however
   # %dorng% loops are reproducible whereas %dopar% loops are not,
   # %dorng% returns the whole sequence of RNG seeds as an attribute whereas %dopar% does not.
   rkhats_boot = foreach::foreach(iboot = 1:nboot, .combine = rbind) %dorng% {
-    simulated_genotype_pair <- do.call(simulate_Ys, args = c(list(frequencies, distances, k = khat, r = rhat), sim_params))
-    rkhats_boot <- estimate_r_and_k(frequencies, distances, Ys = simulated_genotype_pair, ...)
+    simulated_Ys <- do.call(simulate_Ys, args = c(list(fs, ds, k = khat, r = rhat), sim_params))
+    rkhats_boot <- estimate_r_and_k(fs, ds, Ys = simulated_Ys, ...)
   }
 
-  # Calculata CIs as quantiles
+  # Calculate CIs as quantiles of the parametric bootstrap draw
   alpha <- (1-confidence/100)
   CI <- apply(rkhats_boot, 2, quantile, probs = c(alpha/2, 1-alpha/2))
-  if(any(is.na(CI))){stop('Bootstrap CI is NA')}
+  if (any(is.na(CI))) stop('Bootstrap CI is NA')
 
-  # Return CIs
+  # End of function
   return(t(CI))
 }
 
